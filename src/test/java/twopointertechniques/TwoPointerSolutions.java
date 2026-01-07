95982e0b014e053ff92fd8e5a30a7e4b7cacb0db
naveenmadireddy1213 <naveen.madireddy1213@gmail.com>
2026-01-07 22:16:20 +0000

    Added couple more pointer questions

--- src/test/java/twopointertechniques/TwoPointerSolutions.java
+++ src/test/java/twopointertechniques/TwoPointerSolutions.java
@@ -54,31 +54,6 @@ public class TwoPointerSolutions {
         System.out.println(palindrome + " is a palindrome");
     }
 
-    @Test //Need to revisit
-    public void removeDuplicatesFromSortedArray() {
-        int[] nums = {1, 1, 2, 2, 2, 3, 4, 4}; // Sorted input
-
-        if (nums.length == 0) return;
-
-        // 'i' is the slow pointer
-        int p = 0;
-
-        // 'j' is the fast pointer
-        for (int i = 1; i < nums.length; i++) {
-            // If we find a new unique element
-            if (nums[i] != nums[p]) {
-                p++;           // Move unique pointer forward
-                nums[p] = nums[i]; // Update the next unique spot
-            }
-        }
-
-        // Results: The unique elements are in the first (i + 1) positions
-        System.out.println("Number of unique elements: " + (p + 1));
-        System.out.print("Array after removing duplicates: ");
-        for (int k = 0; k <= p; k++) {
-            System.out.print(nums[k] + " ");
-        }
-    }
 
     @Test
     public void reverseWithKeepingSpaces() {
@@ -148,5 +123,30 @@ public class TwoPointerSolutions {
         System.out.println(Arrays.toString(nums));
     }
 
+    @Test //Need to revisit
+    public void removeDuplicatesFromSortedArray() {
+        int[] nums = {1, 1, 2, 2, 2, 3, 4, 4}; // Sorted input
+
+        if (nums.length == 0) return;
+
+        // 'i' is the slow pointer
+        int p = 0;
+
+        // 'j' is the fast pointer
+        for (int i = 1; i < nums.length; i++) {
+            // If we find a new unique element
+            if (nums[i] != nums[p]) {
+                p++;           // Move unique pointer forward
+                nums[p] = nums[i]; // Update the next unique spot
+            }
+        }
+
+        // Results: The unique elements are in the first (i + 1) positions
+        System.out.println("Number of unique elements: " + (p + 1));
+        System.out.print("Array after removing duplicates: ");
+        for (int k = 0; k <= p; k++) {
+            System.out.print(nums[k] + " ");
+        }
+    }
 
 }
